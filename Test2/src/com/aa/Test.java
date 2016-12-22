b4809816f3e8cee379b8dfc88dafeb6fbf0ba5ac
admin <admin@192.168.207.2>
2016-12-22 15:11:59 +0800

    55555555

--- Test2/src/com/aa/Test.java
+++ Test2/src/com/aa/Test.java
@@ -8,7 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Map<String, String> params =new HashMap<String, String>();
-		System.out.println(HttpUtils.sendPost("http:11//116.55.227.145:8070/trade/login","username=admin&password=123456","utf-8"));
+		System.out.println(HttpUtils.sendPost("http:111255555555555512//116.55.227.145:8070/trade/login","username=admin&password=123456","utf-8"));
 	}
 
 }
